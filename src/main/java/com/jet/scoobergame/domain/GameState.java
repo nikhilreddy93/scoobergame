ba10ff7a7e3ce5f85613c80a5169049609d39831
Nikhil <nikhilreddy2493@gmail.com>
2024-08-30 18:06:36 +0200

    Add oldNumber and addedNumber to GameState

--- src/main/java/com/jet/scoobergame/domain/GameState.java
+++ src/main/java/com/jet/scoobergame/domain/GameState.java
@@ -5,17 +5,44 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class GameState {
     private final int number;
+    private int oldNumber = 0;
+    private int addedNumber = 0;
 
     @JsonCreator
     public GameState(@JsonProperty("number") int number) {
         this.number = number;
     }
 
+    public void setOldNumber(int newNumber) {
+        this.oldNumber = newNumber;
+    }
+
+    public void setAddedNumber(int addedNumber) {
+        this.addedNumber = addedNumber;
+    }
+
     public int getNumber() {
         return number;
     }
 
+    public int getOldNumber() {
+        return oldNumber;
+    }
+
+    public int getAddedNumber() {
+        return addedNumber;
+    }
+
     public boolean isGameOver() {
         return number == 1;
+    }
+
+    @Override
+    public String toString() {
+        return "GameState{" +
+                "number=" + number +
+                ", newNumber=" + oldNumber +
+                ", addedNumber=" + addedNumber +
+                '}';
     }
 }
